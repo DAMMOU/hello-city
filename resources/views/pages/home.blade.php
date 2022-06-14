8b6896ede0728565a05fca234759c20d1edacef1
DAMMOU <udammou@gmail.com>
2022-06-14 20:00:39 +0200

    End of Project

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,7 +6,7 @@
     <div class="flex flex-col justify-center items-center">
        <img src="{{asset('/images/berberlogo.png')}}" 
        class="mt-12 shadow-md h-32"/>
-       <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Marroco!</h1>
+       <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Morocco!</h1>
        <p class="mt-4 text-lg text-gray-800">It's currenty  {{date('h:i A')}}</p>
     </div>
 @endsection
